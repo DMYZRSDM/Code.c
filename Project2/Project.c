74e42f7039368d0afb840fbca0aafd9d40cbb0b3
YYf <1191593067@qq.com>
2023-05-09 20:28:38 +0800

    5-9

--- Project2/Project.c
+++ Project2/Project.c
@@ -9,26 +9,59 @@
 #include"add.h"
 #include"game.h"
 
-int fac(int n)
+void swap(int* pt1, int* pt2)
 {
-	if (n < 0)
-	{
-		printf("input error");
-	}
-	if (n == 0)
-		return 1;
-	else
-		return(n * fac(n - 1));
+	int temp = 0;
+	temp = *pt1;
+	*pt1 = *pt2;
+	*pt2 = temp;
+}
+void exchange(int* q1, int* q2, int* q3)
+{
+	void swap(int* pt1, int* pt2);
+	if (*q1 < *q2)
+		swap(q1, q2);
+	if (*q1 < *q3)
+		swap(q1, q3);
+	if (*q2 < *q3)
+		swap(q2, q3);
 }
 int main()
 {
-	int n,num;
-	printf("input an integer number:");
-	scanf("%d", &n);
-	num = fac(n);
-	printf("%d",num);
+	void exchange(int* q1, int* q2, int* q3);
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int* p1, * p2, * p3;
+	printf("Please enter three numbers:");
+	scanf("%d,%d,%d", &a, &b, &c);
+	p1 = &a;
+	p2 = &b;
+	p3 = &c;
+	exchange(p1, p2, p3);
+	printf("The order is:%d,%d,%d\n", a, b, c);
 	return 0;
 }
+//int fac(int n)
+//{
+//	if (n < 0)
+//	{
+//		printf("input error");
+//	}
+//	if (n == 0)
+//		return 1;
+//	else
+//		return(n * fac(n - 1));
+//}
+//int main()
+//{
+//	int n,num;
+//	printf("input an integer number:");
+//	scanf("%d", &n);
+//	num = fac(n);
+//	printf("%d",num);
+//	return 0;
+//}
 //int num(int x)
 //{
 //	/*int i;
