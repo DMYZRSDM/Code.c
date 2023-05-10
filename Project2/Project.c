0dc8fe859ed0c13a54a9a8257b703da4c4b67470
YYf <1191593067@qq.com>
2023-05-10 22:33:43 +0800

    5-10

--- Project2/Project.c
+++ Project2/Project.c
@@ -9,39 +9,44 @@
 #include"add.h"
 #include"game.h"
 
-void swap(int* pt1, int* pt2)
-{
-	int temp = 0;
-	temp = *pt1;
-	*pt1 = *pt2;
-	*pt2 = temp;
-}
-void exchange(int* q1, int* q2, int* q3)
-{
-	void swap(int* pt1, int* pt2);
-	if (*q1 < *q2)
-		swap(q1, q2);
-	if (*q1 < *q3)
-		swap(q1, q3);
-	if (*q2 < *q3)
-		swap(q2, q3);
-}
 int main()
 {
-	void exchange(int* q1, int* q2, int* q3);
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int* p1, * p2, * p3;
-	printf("Please enter three numbers:");
-	scanf("%d,%d,%d", &a, &b, &c);
-	p1 = &a;
-	p2 = &b;
-	p3 = &c;
-	exchange(p1, p2, p3);
-	printf("The order is:%d,%d,%d\n", a, b, c);
+
 	return 0;
 }
+//void swap(int* pt1, int* pt2)
+//{
+//	int temp = 0;
+//	temp = *pt1;
+//	*pt1 = *pt2;
+//	*pt2 = temp;
+//}
+//void exchange(int* q1, int* q2, int* q3)
+//{
+//	void swap(int* pt1, int* pt2);
+//	if (*q1 < *q2)
+//		swap(q1, q2);
+//	if (*q1 < *q3)
+//		swap(q1, q3);
+//	if (*q2 < *q3)
+//		swap(q2, q3);
+//}
+//int main()
+//{
+//	void exchange(int* q1, int* q2, int* q3);
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int* p1, * p2, * p3;
+//	printf("Please enter three numbers:");
+//	scanf("%d,%d,%d", &a, &b, &c);
+//	p1 = &a;
+//	p2 = &b;
+//	p3 = &c;
+//	exchange(p1, p2, p3);
+//	printf("The order is:%d,%d,%d\n", a, b, c);
+//	return 0;
+//}
 //int fac(int n)
 //{
 //	if (n < 0)
