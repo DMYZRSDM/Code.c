e842f46de01e9d42adbddbc79db1a8a3e20ebdd7
YYf <1191593067@qq.com>
2023-05-27 20:42:09 +0800

    5-27

--- Project2/Project.c
+++ Project2/Project.c
@@ -11,40 +11,48 @@
 
 int main()
 {
-	return 0;
-}
-void search(float (*p)[4],int n)
-{
+	char* name[] = { "Follow me","Great Wall","FORTRAN" };
+	char** p;
 	int i = 0;
-	int j = 0;
-	int flag = 0;
-	for (j = 0; j < n; j++)
+	for (i = 0; i < 5; i++)
 	{
-		flag = 0;
-		for (i = 0; i < 4; i++)
-		{
-			if (*(*(p + j) + i) < 60)
-				flag = 1;
-		}
-		if (flag == 1)
-		{
-			printf("No.%d fails,his scores are:\n", j + 1);
-			for (i = 0; i < 4; i++)
-			{
-				printf("%5.1f", *(*(p + j) + i));
-				
-			}
-			printf("\n");
-		}
+		p = name + i;
+		printf("%s\n", *p);
 	}
-}
-int main()
-{
-	void search(float(*p)[4], int n);
-	float score[3][4] = { {65,57,70,60},{58,87,89,84},{90,99,100,98} };
-	search(score, 3);
 	return 0;
 }
+//void search(float (*p)[4],int n)
+//{
+//	int i = 0;
+//	int j = 0;
+//	int flag = 0;
+//	for (j = 0; j < n; j++)
+//	{
+//		flag = 0;
+//		for (i = 0; i < 4; i++)
+//		{
+//			if (*(*(p + j) + i) < 60)
+//				flag = 1;
+//		}
+//		if (flag == 1)
+//		{
+//			printf("No.%d fails,his scores are:\n", j + 1);
+//			for (i = 0; i < 4; i++)
+//			{
+//				printf("%5.1f", *(*(p + j) + i));
+//				
+//			}
+//			printf("\n");
+//		}
+//	}
+//}
+//int main()
+//{
+//	void search(float(*p)[4], int n);
+//	float score[3][4] = { {65,57,70,60},{58,87,89,84},{90,99,100,98} };
+//	search(score, 3);
+//	return 0;
+//}
 //int Lmax(int x, int y)
 //{
 //	int z = 0;
